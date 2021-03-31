99f18c81df0b4d0e2e39a72d13627337bf692785
madan dale <madandale@madans-MacBook-Pro.local>
2021-03-31 22:03:38 +0530

    new code check in

---
+++ src/App.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class App {
+    private static final String List = null;
+
+    public static void main(String[] args) throws Exception {
+   
+        
+
+List <String> employeeName = new ArrayList<String>(); 
+
+        employeeName.add("Santosh");
+        employeeName.add("Bhushan");
+        employeeName.add("Vinayak");
+        employeeName.add("Madan");
+        employeeName.add("Bhushan");
+
+
+        for (String name :employeeName) {
+
+            System.out.println("name : "+name);
+        }
+
+
+    }
+}
